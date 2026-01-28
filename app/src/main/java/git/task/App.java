7d9534007e6748b94a8aa4888d53ead4f9f876c0
rhush <rbolante912@gmail.com>
2026-01-28 14:59:04 +0800

    Added my name

--- app/src/main/java/git/task/App.java
+++ app/src/main/java/git/task/App.java
@@ -3,6 +3,8 @@ package git.task;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello!");
+        System.out.println("My name is Russel Bolante");
+        System.out.println("How are you today?");
     }
 }
